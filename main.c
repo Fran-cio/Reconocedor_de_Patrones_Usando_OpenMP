29c787b4102dec82082536d0dee6b49114f28a59
Francisco Ciordia <gatupancho@gmail.com>
2022-04-29 22:55:10 -0300

    build: -descartado bmp, pgm funcionando

--- main.c
+++ main.c
@@ -1,62 +1,40 @@
+#include "./src/PGM_in_c.c"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main ()
 {
-  FILE* fp = fopen("./img/DSC_2337_baw.bmp","rb");
-  // FILE* fp = fopen("./img/test.bmp","rb");   //read the file//
-  // FILE* fp = fopen("./img/negro.bmp","rb");   //read the file//
+  const char* imagen_base= "./img/DSC_2337_baw.pgm";
+  const char* imagen_cortada = "./img/test.pgm";
 
-  if(fp==NULL)
+  PGMImage* str_imagen_base = malloc(sizeof(PGMImage));
+  PGMImage* str_imagen_cortada = malloc(sizeof(PGMImage));
+
+
+
+  // Process the image and print
+  // its details
+  if (openPGM(str_imagen_base, imagen_base))
+    printImageDetails(str_imagen_base, imagen_base);
+
+  if (openPGM(str_imagen_cortada, imagen_cortada))
+    printImageDetails(str_imagen_cortada, imagen_cortada);
+
+  int maxsize = str_imagen_cortada -> width * str_imagen_cortada ->height;
+  for(int i=1;i<=maxsize;i++)
   {
-    perror("Error abriendo imagen");
-    exit(EXIT_FAILURE);
+    printf("%d|",str_imagen_cortada ->data[i-1]);
+    if(i%str_imagen_cortada ->width ==0)
+      printf("\n");
   }
 
-  unsigned char imageHeader[54];
-
-  for(int i=0;i<54;i++)//read the 54 byte header from fIn
-  {									
-    imageHeader[i] = (unsigned char) getc(fp);								
-  }
-
-  int width = *(int*)&imageHeader[18];
-  int height = *(int*)&imageHeader[22];
-  short int bitDepth = *(short int*)&imageHeader[28];
-  int colorused = *(int*)&imageHeader[46];
-
-  printf("w:%d,h:%d,D:%d,cn:%d\n",width,height,bitDepth,colorused);
-
-  if(bitDepth <= 8)	// COLOR TABLE Present
-    fseek(fp, 4 * colorused, SEEK_CUR);
-
-
-  int imgDataSize = width * height;		//calculate image size
-
-  short int temp, matriz_mono[imgDataSize];   		
-
-  short int azul,
-        verde,
-        rojo;
-
-  for(int i=0; i<imgDataSize;i++)
+  int maxsiz = str_imagen_base ->width * str_imagen_base ->height;
+  for(int i=1;i<=maxsiz;i++)
   {
-    azul = (short int)getc(fp);                   //blue
-    verde = (short int)getc(fp);                   //green
-    rojo = (short int)getc(fp);                   //red
-
-    temp =(short int) ((rojo * 0.3) + (verde * 0.59) + (azul * 0.11)); //conversion formula of rgb to gray
-
-    matriz_mono[i] = temp;
+    printf("%d|",str_imagen_base ->data[i-1]);
+    if(i%str_imagen_base ->width ==0)
+      printf("\n");
   }
-  int i=*matriz_mono;
-  i=i;
-  // for (int i=1; i<=imgDataSize; i++)
-  // {
-  //   printf("%d |",(int)matriz_mono[i-1]);
-  //   if(i%width==0)
-  //     printf("\n");
-  // }
-  fclose(fp);
+
   return 0;
 }
